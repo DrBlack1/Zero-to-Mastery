c0aef2a7b1855d601932cffafb2b8fa92b89f470
yourUserName <patrick.mack1@yahoo.com>
2019-04-19 22:25:24 +0100

    Create server.js

---
+++ Environmental Variables/server.js
@@ -0,0 +1,9 @@
+const app = require('http')
+    .createServer((req, res) => res.sendDate('oh hi there!'));
+
+const DATABASE_URL = process.env.DATABASE_URL   
+app.listen(3000, () => {
+    console.log(`Server is listening on port ${DATABASE_URL}`);
+});
+
+console.log(3000)
