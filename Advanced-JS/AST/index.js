4ce1bd44c632d9336529163e8323526cf666feb7
yourUserName <patrick.mack1@yahoo.com>
2019-05-11 16:35:38 +0100

    Create index.js

---
+++ Advanced-JS/AST/index.js
@@ -0,0 +1,13 @@
+let tips = [
+    "Click on any AST node with a '+' to expand it",
+  
+    "Hovering over a node highlights the \
+     corresponding part in the source code",
+  
+    "Shift click on an AST node expands the whole substree"
+  ];
+  
+  function printTips() {
+    tips.forEach((tip, i) => console.log(`Tip ${i}:` + tip));
+  }
+  
