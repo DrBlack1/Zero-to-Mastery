d50c43c2e661b10d6db794e450ba25dfefcade82
yourUserName <patrick.mack1@yahoo.com>
2019-05-11 15:16:29 +0100

    Update ztm.js

--- Advanced-JS/Garbage Collection/ztm.js
+++ Advanced-JS/Garbage Collection/ztm.js
@@ -1,15 +1,16 @@
 //Call Stack + Memory Heap
 const number = 610; //allocate memory for number
 const string = 'some text' // allocate memory for a string
-let human = { // allocate memoty for an object.... and it's values.
-    first: 'Patrick',
-    last: 'Mack'
-}
 
-human = 5;
+
+
 
 
 function subtractTwo(num) {
+    var human = { // allocate memoty for an object.... and it's values.
+        first: 'Patrick',
+        last: 'Mack'
+    }
     return num - 2;
 }
 
