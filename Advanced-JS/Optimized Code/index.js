a84f07918833043903f2e9b3cd5308dcc603979e
yourUserName <patrick.mack1@yahoo.com>
2019-05-11 14:40:51 +0100

    Optimized code

---
+++ Advanced-JS/Optimized Code/index.js
@@ -0,0 +1,11 @@
+// inline caching
+// function findUser(user) {
+//     return `found ${user.firstName} ${user.lastName}`
+// }
+
+// const userData {
+//     firstName: 'Patrick'
+//     lastName: 'Mack'
+// }
+
+// findUser(userData)
