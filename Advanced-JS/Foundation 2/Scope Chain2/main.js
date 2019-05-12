8375858b9ee94890201a2da271a0cbe5499ad4bb
yourUserName <patrick.mack1@yahoo.com>
2019-05-12 22:13:04 +0100

    Create main.js

---
+++ Advanced-JS/Foundation 2/Scope Chain2/main.js
@@ -0,0 +1,16 @@
+'use strict'
+function weird() {
+    var height = 50;
+    return height
+}
+
+weird()
+
+
+var heyhey = function doodle() {
+    //do something
+    doodle()
+    return 'heyhey'
+}
+
+heyhey()
