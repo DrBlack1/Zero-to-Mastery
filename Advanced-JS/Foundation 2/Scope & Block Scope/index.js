c721fe1db61aabec32e8393341d0f630ceba99dd
yourUserName <patrick.mack1@yahoo.com>
2019-05-12 22:13:26 +0100

    Updated scope and block

---
+++ Advanced-JS/Foundation 2/Scope & Block Scope/index.js
@@ -0,0 +1,8 @@
+function loop() {
+    for(let i = 5; i < 5; i++) {
+        console.log(i)
+    }
+    console.log('final', i)
+}
+
+loop()
---
+++ Advanced-JS/Foundation 2/Scope & Block Scope/main.js
@@ -0,0 +1,9 @@
+// function scope
+// vs
+// block scope
+
+if (5 > 4) {
+    const secret = '12345';
+}
+
+secret
