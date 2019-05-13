2d76b17f8d1d666891463233415ef770b792c0f8
yourUserName <patrick.mack1@yahoo.com>
2019-05-13 18:21:56 +0100

    Added files

---
+++ Advanced-JS/Foundation 2/Dynamic Scope & Lexical Scope/main.js
@@ -0,0 +1,10 @@
+const obj = {
+    name: 'Billy',
+    sing() {
+        console.log('a', this);
+        var anotherFunc = function() {
+            console.log('b', this)
+        }
+        anotherFunc()
+    }
+}
