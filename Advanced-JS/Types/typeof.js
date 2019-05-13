b2ca80210a9315b2ba4242edf4d49eebf4ee18ca
yourUserName <patrick.mack1@yahoo.com>
2019-05-13 22:08:57 +0100

    Create typeof.js

---
+++ Advanced-JS/Types/typeof.js
@@ -0,0 +1,18 @@
+//Primitive
+typeof 5
+typeof true
+typeof 'To be or not to be'
+typeof undefined
+typeof null
+typeof Symbol('just me')
+
+//Non=Primitive
+const obj1 = {
+    a: 'Tom'
+}
+typeof {}
+typeof []
+typeof function(){}
+
+String('true').toString()
+typeof Infinity
