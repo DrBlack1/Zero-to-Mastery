0dcaf469390c00ac26dc3b749105cb5dcbde857a
yourUserName <patrick.mack1@yahoo.com>
2019-05-14 18:11:11 +0100

    Create higherOrder2.js

---
+++ Advanced-JS/2 Pillars Closures & Prototypal Inheritance/higherOrder2.js
@@ -0,0 +1,28 @@
+const giveAccessTo = (name) =>
+    'Access Greanted to ' + name
+
+function authenticate(verify) {
+    let array = [];
+    for (let i = 0; i < verify; i++) {
+        array.push(i)
+    }
+    return giveAccessTo(person.name)
+}
+function sing(person) {
+    return 'la lal la my name is ' + person.name;
+}
+function letPerson(person, fn) { // ++ tell it what data to user + function.
+    if (person.leverl === 'admin') {
+        return fn(person)
+    } else if (person.level === 'user') {
+        return fn(person)
+    }
+}
+
+letPerson({level: 'user', name: 'Sally', sing})
+
+
+const multiplyBy
+
+multiplyTwo
+multiplyByTen
