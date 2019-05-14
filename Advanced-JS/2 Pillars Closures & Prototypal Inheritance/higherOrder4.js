a7c303662a3970bb6ba4808d69f2a410ac9120b2
yourUserName <patrick.mack1@yahoo.com>
2019-05-14 18:25:42 +0100

    Create higherOrder4.js

---
+++ Advanced-JS/2 Pillars Closures & Prototypal Inheritance/higherOrder4.js
@@ -0,0 +1,8 @@
+const multiplyBy = (num1) => (num2) => num1*num2
+
+multiplyBy(4)(6)
+// const multiplyByTwo = multiplyBy(2);
+// const multiplyByFive = multiplyBy(5);
+// multiplyByTwo(4)
+// multiplyByTwo(10)
+// multiplyByFive(6)
