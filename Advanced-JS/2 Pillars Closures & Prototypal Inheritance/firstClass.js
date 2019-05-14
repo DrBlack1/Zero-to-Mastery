a5d7154fd3de8af64de882ecdc2e7c6a835ac451
yourUserName <patrick.mack1@yahoo.com>
2019-05-14 17:29:56 +0100

    Create firstClass.js

---
+++ Advanced-JS/2 Pillars Closures & Prototypal Inheritance/firstClass.js
@@ -0,0 +1,16 @@
+//Functions are frst class citizens in JS
+//1
+var stuff = function(){}
+//2
+function a(fn) {
+    fn()
+}
+
+a(function() {console.log('hi there')})
+//3
+function b() {
+    return function c() {console.log('bye')}
+}
+
+var d = b()
+d()
