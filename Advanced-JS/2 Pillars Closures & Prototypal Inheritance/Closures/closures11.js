3912fded1d4e7e6c01379dbe329c801233895005
yourUserName <patrick.mack1@yahoo.com>
2019-05-14 22:05:18 +0100

    Create closures11.js

---
+++ Advanced-JS/2 Pillars Closures & Prototypal Inheritance/Closures/closures11.js
@@ -0,0 +1,18 @@
+let view;
+function initialize() {
+    let called = 0;
+    return function() {
+        if (called > 0) {
+            return;
+        } else {
+        view = '🏔';
+        called++;
+        console.log('view has been set!')
+        }
+    }  
+}
+const startOnce = initialize();
+startOnce();
+startOnce();
+startOnce();
+console.log(view)
