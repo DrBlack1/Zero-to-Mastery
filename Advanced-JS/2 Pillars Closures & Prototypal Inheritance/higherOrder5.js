f58e1b712165c071329546aa7a2b9e16546c51d2
yourUserName <patrick.mack1@yahoo.com>
2019-05-14 18:26:58 +0100

    Create higherOrder5.js

---
+++ Advanced-JS/2 Pillars Closures & Prototypal Inheritance/higherOrder5.js
@@ -0,0 +1,6 @@
+// Make a generic multiplyBy HOF that can make other functions like: multiplyByTwo, multiplyByTen and so on...
+
+
+// You should be able to do:
+const multiplyByTwo = multiplyBy(2);
+multiplyByTwo(4) // 8
