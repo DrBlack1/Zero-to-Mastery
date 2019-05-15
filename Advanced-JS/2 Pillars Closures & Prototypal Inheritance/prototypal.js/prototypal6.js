a99a5e8b4c43541091a9271db9166067b0f1cc96
yourUserName <patrick.mack1@yahoo.com>
2019-05-15 18:20:24 +0100

    Create prototypal6.js

---
+++ Advanced-JS/2 Pillars Closures & Prototypal Inheritance/prototypal.js/prototypal6.js
@@ -0,0 +1,13 @@
+//Exercise - extend the functionality of a built in object
+
+//#1
+// Date object => to have new method .lastYear()
+// which shows you last year 'YYYY' format.
+
+new Date('1900-10-10').lastYear()
+//'1899'
+
+//#Bonus
+// Mofify .map() to print 'cal' at the end of each ite.
+console.log([1,2,3].map())
+//1cal, 2cal, 3cal
