7a379e2059ece349d4d7ee171ee123a2e1b0cece
yourUserName <patrick.mack1@yahoo.com>
2019-05-17 21:10:57 +0100

    Added js file

--- Advanced-JS/Functional Programming/arity.js
+++ Advanced-JS/Functional Programming/arity.js
@@ -1,3 +1,7 @@
+//arity
+
+
+
 fn1(fn2(fn3(50)));
 CompositionEvent(fn1, fn2, fn3)(50)
 pipe(fn3, fn2, fn1)(50)
@@ -7,7 +11,5 @@ pipe(fn3, fn2, fn1)(50)
 const compose = (f, g) => (data) => f(g(data))
 const pipe = (f, g) => (data) => g(f(data))
 const multiplyBy3 = (num) => num*3;
-const makePostitive = (num) => Math.abs(num);
-const multiplyBy3AndAbsolute = compose(multiplyBy3, makePostitive)
-
-multiplyBy3AndAbsolute(-50)
+const makePositive = (num) => Math.abs(num);
+const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive)
