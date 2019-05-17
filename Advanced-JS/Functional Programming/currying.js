ec78b6c54feae94a020d9ffa4d515e939f524628
yourUserName <patrick.mack1@yahoo.com>
2019-05-17 18:12:48 +0100

    Create currying.js

---
+++ Advanced-JS/Functional Programming/currying.js
@@ -0,0 +1,9 @@
+// currying
+const multiply = (a, b) => a*b;
+const curriedMultiply = (a) => (b) => a*b;
+const curriedMultiplyBy5 = curriedMultiply(5);
+
+//10 years
+curriedMultiplyBy5(4)
+curriedMultiplyBy5(4)
+curriedMultiplyBy5(4)
