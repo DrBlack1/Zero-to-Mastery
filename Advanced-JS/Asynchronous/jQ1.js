a32ae203bf773b1c49ba969c2702d31c91446390
yourUserName <patrick.mack1@yahoo.com>
2019-05-17 22:37:46 +0100

    Updated js file

--- Advanced-JS/Asynchronous/jQ1.js
+++ Advanced-JS/Asynchronous/jQ1.js
@@ -1,9 +1,8 @@
-//Callback Queue - Task Queue
 setTimeout(()=>{console.log('1', 'is the loneliest number')}, 0)
 setTimeout(()=>{console.log('2', 'can be as bad as one')}, 10)
 
-//2 Job Queue - Microtask Queue
+//2
 Promise.resolve('hi').then((data)=> console.log('2', data))
 
 //3
-console.log('3', 'is a crowd')
+console.log('3','is a crowd')
