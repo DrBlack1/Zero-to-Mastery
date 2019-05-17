6d6230d8e40a62c8f07a754e62c7d44c90f03e63
yourUserName <patrick.mack1@yahoo.com>
2019-05-17 21:38:01 +0100

    Create amazon2.js

---
+++ Advanced-JS/Functional Programming/amazon2.js
@@ -0,0 +1,54 @@
+// Amazon shopping
+const user = {
+    name: 'Kim',
+    active: true,
+    cart: [],
+    purchases: []
+  }
+  const amazonHistory = []
+  const compose = (f,g) => (...args) => g(f(...args));
+  purchaseItem(
+      emptyCart,
+      buyItem,
+      applyTaxToItems,
+      addItemToCart
+  )(user, {name: 'laptop', price: 200})
+  
+  function purchaseItem(...fns) {
+    return fns.reduce(compose)
+  }
+
+  function addItemToCart(user, item) {
+    amazonHistory.push(user)
+    const updateCart = user.cart.concat(item)
+    return Object.assign({}, user, { cart: updateCart})
+  }
+
+  function applyTextToItems(user) {
+    amazonHistory.push(user)
+    const {cart} = user;
+    const taxRate = 1.3;
+    const updateCart = cart.map(item => {
+        return {
+            name: item.name,
+            price: item.price*taxRate
+        }
+    })
+    return Object.assign({}, user, { cart: updatedCart})
+  }
+
+  function buyItem(user) {
+    amazonHistory.push(user)
+    return Object.assign({}, user, { purchases: user.cart})
+  }
+
+  function emptyCart(user) {
+    amazonHistory.push(user)
+    return Object.assign({}, user, {cart: []})
+  }
+
+  //Implement a cart feature:
+  // 1. Add items to cart.
+  // 2. Add 3% tax to item in cart
+  // 3. Buy item: cart --> purchases
+  // 4. Empty cart
