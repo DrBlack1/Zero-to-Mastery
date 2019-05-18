a79e887e098d0c44561b0121d21f901e4fd401ce
yourUserName <patrick.mack1@yahoo.com>
2019-05-18 11:47:40 +0100

    Update script.js

---
+++ Advanced-JS/Modules/ES6 Modules/script.js
@@ -0,0 +1,33 @@
+// CommonJS and AMD
+var module1 = require('module1') //.fight;
+var module2 = require('module2') //.importedFunc2;
+
+function fight() {
+
+}
+
+module.exports = {
+    fight: fight
+};
+
+
+
+
+
+
+
+//IIFE
+//Module Pattern
+var fightModule = (function() {
+    var harry = 'potter'
+    var voldemort = 'He who must not be named'
+    
+    function fight(char1, char2) {
+        var attack1 = Math.floor(Math.random() * char1.length);
+        var attack2 = Math.floor(Math.random() * char2.length);
+        return attack1 > attack2 ? `${char1} wins` : `${char2} wins`
+    }
+    return {
+        fight: fight
+    }
+})();
