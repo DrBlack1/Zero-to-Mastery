7a366f8733ccb673566f4694ac41ca56cb2bf28f
yourUserName <patrick.mack1@yahoo.com>
2019-05-18 13:32:57 +0100

    Create async.js

---
+++ Advanced-JS/Error Handling/async.js
@@ -0,0 +1,13 @@
+Promise.resolve('asyncfail')
+    .then(response => {
+        Promise.resolve().then(()=> {
+            throw new Error('#3 fail')
+        }).catch(console.log)
+        return 5
+    })
+    .then(response => {
+        console.log(response)
+    })
+    .catch(err => {
+        console.log('final error')
+    })
