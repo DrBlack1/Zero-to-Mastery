357d60c9bbe0dc14c28a7af53dccae51c9de2ed5
yourUserName <patrick.mack1@yahoo.com>
2019-05-18 20:58:57 +0100

    Update script.js

--- Advanced-JS/Appendix1/script.js
+++ Advanced-JS/Appendix1/script.js
@@ -2,6 +2,14 @@ var database = [
     {
         username: "patrick",
         password: "supersecret"
+    },
+    {
+        username: "sally",
+        password: "123"
+    },
+    {
+        username: "ingrid",
+        password: "777"
     }
 ];
 
@@ -14,18 +22,31 @@ var newsFeed = [
         username: "Sally",
         timeline: "Javascript is sooooo coool!"
     },
+    {
+        username: "Mitch",
+        timeline: "Javascript is preeetyy cool!"
+    }
 ];
 
-var userNamePrompt = prompt("What's your username?");
-var passwordPrompt = prompt("What's your password?");
+function isUserValid(username, password) {
+    for (var i = 0; i < database.length; i++) {
+        if(database[i].username === username &&
+            database[i].password === password) {
+            return true;
+        }
+    }
+    return false;
+}
 
-function signIn(user, pass) {
-    if (user === database[0].username &&
-        pass === database[0].password) {
+function signIn(username, password) {
+    if (isUserValid(username, password)) {
             console.log(newsFeed);
         } else {
             alert("Sorry, wrong username and password!");
         }
 }
+
+var userNamePrompt = prompt("What\'s your username?");
+var passwordPrompt = prompt("What\'s your password?");
 
 signIn(userNamePrompt, passwordPrompt);
