8721eb1f5e36cbdc0e9b7a230c5ca868bbfdb570
yourUserName <patrick.mack1@yahoo.com>
2019-05-18 11:44:57 +0100

    Create main.js

---
+++ Advanced-JS/Modules/commonJS, AMD, UMD/main.js
@@ -0,0 +1,13 @@
+define(['module1', module2],
+    function (module1Import, module2Import) {
+        var module1 = module1Import //.fight;
+        var module2 = module2Import //.importedFunc2;
+
+        function dance() {
+
+        }
+
+        return {
+            dance: dance,
+        };
+    });
