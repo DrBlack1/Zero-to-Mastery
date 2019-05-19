7068371b94733ccd69d8720a15e97da4e4701e81
yourUserName <patrick.mack1@yahoo.com>
2019-05-19 17:14:48 +0100

    Create controlFlow.js

---
+++ Advanced-JS/Appendix1/controlFlow.js
@@ -0,0 +1,53 @@
+//JS Brower Console Control Flow
+// if statement
+var name = "Billy";
+if (name === "Billy") {
+    alert("hi Billy!");
+}
+
+// else statement
+name = "Billy"; //name
+if (name === "Billy") {
+    alert("hi Billy!");
+} else {
+    alert("hmmm I dont know you");
+}
+
+// else if statement
+name = "Susy";
+if (name === "Billy") {
+    alert("hi Billy!");
+} else if (name === "Susy") {
+    alert("hi Susy!");
+} else {
+    alert("I dont know you");
+}
+
+// || statement
+name = "Billy"
+name = "Ann"
+if (name === "Billy" || name === "Ann") {
+    alert("Hi Billy or Ann");
+}
+
+// && statement
+if (name === "Billy" && name === "Ann") {
+    alert("Hi Billy or Ann");
+}
+
+// Concat statement
+var firstName = "Bob";
+var lastName = "Smith";
+if (firstName === "Bob" && lastName === "Smith") {
+    alert("Hi Bob Smith");
+}
+
+// Opersite ! statement
+if (!(name === "Bob")) {
+    alert("Hi Bob");
+}
+
+!false
+true
+!true
+false
