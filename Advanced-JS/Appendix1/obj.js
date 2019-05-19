50b64586c1d3497b79b7e72cee9512a8f6b53358
yourUserName <patrick.mack1@yahoo.com>
2019-05-19 20:37:24 +0100

    Create obj.js

---
+++ Advanced-JS/Appendix1/obj.js
@@ -0,0 +1,32 @@
+// Objects collection of property
+var user = {
+    name: "John",
+    age: 34,
+    hobby: "Soccer",
+    isMarried: false,
+    spells: ["abrakadra", "shazam", "boo"],
+    shout: function() {
+        console.log("AHHHHHH!");
+    }
+};
+
+var list = [
+    {
+        username: "andy",
+        password: "secre"
+    },
+    {
+        username: "jess",
+        password: "123"
+    },
+];
+
+// var list = ["apple", "banana", "orange"];
+
+// user.favouriteFood = "spinach";
+// "spinach"
+// user
+
+// user.isMarried = true
+// true
+// user
