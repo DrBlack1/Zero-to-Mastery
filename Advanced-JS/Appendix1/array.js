f4adcd19275acec69e8124f94c6bafff73dac451
yourUserName <patrick.mack1@yahoo.com>
2019-05-19 20:01:17 +0100

    Create array.js

---
+++ Advanced-JS/Appendix1/array.js
@@ -0,0 +1,39 @@
+// Array
+var list = [
+    ["tiger", "cat", "bear", "bird"]
+];
+
+console.log(list[0]);
+
+var number = [1,2,3,4];
+
+var booleans = [true, false, true];
+
+var functionList = [function apple() {
+    console.log("apple");
+}]
+
+var mixedList = ["apples", 3, undefined, true, function apple() {
+    console.log("apples")
+}];
+
+//JS Browser Console
+list.shift();
+"tiger"
+list
+
+list.pop();
+"bird"
+list
+
+list.push("elephant");
+3
+list
+
+list.concat(["bee", "deer"]);
+list.sort();
+
+var myList = ["cat", "bear", "elephant", "bee", "deer"];
+myList
+var myNewList = myList.concat(["monkey"]);
+myNewList
