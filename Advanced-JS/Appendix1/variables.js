cf84e20fc3acebed724aab8b9dcd356ae9911c9e
yourUserName <patrick.mack1@yahoo.com>
2019-05-19 13:28:47 +0100

    Create variables.js

---
+++ Advanced-JS/Appendix1/variables.js
@@ -0,0 +1,49 @@
+//JS Brower Console Variables
+"These pretzels are making me thirsty" + "!"
+"These pretzels are making me thirsty!"
+
+var george = "These pretzels are making me thirsty" + "!";
+undefined
+
+george
+"These pretzels are making me thirsty!"
+
+var three = 3
+undefined
+
+three
+3
+
+prompt()
+"Hello" //returns whatever you type in the console
+
+prompt("What is yout username") //type the username
+"patto"
+
+var first = prompt("enter first number");//returns your number in the console
+var second = prompt("enter second number");
+
+first
+5
+second
+10
+
+Number(first)
+5
+var sum = Number(first) + Number(second);
+
+sum
+15
+
+alert("ht there!")
+var first = prompt("enter first number");//enter first number
+var second = prompt("enter second number");//enter second number
+var sum = Number(first) + Number(second);//calulates the numbers
+alert(sum);//alerts the sum
+
+//First Programming
+var first = prompt("enter first number");//enter first number
+var second = prompt("enter second number");//enter second number
+var sum = Number(first) + Number(second);//calulates the numbers
+alert("The sum is:" + sum);//alerts the sum
+
