fccc2d96c528bff7635bbcb517fbe2568658fa62
yourUserName <patrick.mack1@yahoo.com>
2019-05-20 22:22:39 +0100

    Update ecma.js

--- Advanced-JS/Appendix2/ecma.js
+++ Advanced-JS/Appendix2/ecma.js
@@ -1,1 +1,19 @@
 // let + const
+
+const player = 'bobby';
+let experience = 100;
+let wizardLevel = false;
+
+if (experience > 90) {
+    let wizardLevel = true;
+    console.log('inside', wizardLevel);
+}
+
+console.log('outside', wizardLevel);
+
+
+const obj = {
+    player: 'bobby',
+    experience: 100,
+    wizardLevel: false
+}
