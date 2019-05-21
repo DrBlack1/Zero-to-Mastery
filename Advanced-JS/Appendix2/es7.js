3fecfe4e8731d5dd414d00e6e96305da49a87742
yourUserName <patrick.mack1@yahoo.com>
2019-05-21 12:36:32 +0100

    Create es7.js

---
+++ Advanced-JS/Appendix2/es7.js
@@ -0,0 +1,12 @@
+// ES7
+const pets = ['cat', 'dog', 'bat'];
+
+pets.includes('dog');
+// true
+// pets.includes('bird') - false
+
+// Expedential Operator
+const squeare = (x) => x**2
+// square(2) - 4
+const cube = (y) => y**3
+// cube(3) - 27
